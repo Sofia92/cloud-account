800643b3c37daa3b324d46fdcc71b42d776b6675
Sofia92 <1157335914@qq.com>
2018-08-20 15:17:10 +0800

    update modal ui lib

--- src/ui/components/modal/modal.ts
+++ src/ui/components/modal/modal.ts
@@ -4,28 +4,15 @@
  * Version: 1.0.0
  * Description:
  */
-import { Component, HostBinding, Input } from '@angular/core';
+import { Component } from '@angular/core';
 @Component({
   selector: '[apModal]',
   template: `
-    <ng-content></ng-content>`,
+    <div class="modal-content">
+      <ng-content></ng-content>
+    </div>`,
   styleUrls: ['./modal.scss']
 })
 export class ApModal {
-  @Input('open')
-  @HostBinding('class.open')
-  open = false;
 
-  @HostBinding('class.ap-ui-modal')
-  private classSelector = true;
-
-  @HostBinding('style.width')
-  get modalWidth() {
-    return `${window.innerWidth}px`;
-  }
-
-  @HostBinding('style.height')
-  get modalHeight() {
-    return `${window.innerHeight}px`;
-  }
 }
